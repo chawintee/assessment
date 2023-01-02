1d961ab90c697571f3ef2dd6999827696ca70517
chawintee <chawinteetea@gmail.com>
2023-01-02 17:08:01 +0700

    [ADD] get all expenses

--- server.go
+++ server.go
@@ -19,6 +19,7 @@ func main() {
 	e.POST("/expenses", expense.CreateExpensesHandler)
 	e.GET("/expenses/:id", expense.GetExpenseHandler)
 	e.PUT("/expenses/:id", expense.UpdateExpenseHandler)
+	e.GET("/expenses", expense.GetExpensesHandler)
 
 	log.Fatal(e.Start(os.Getenv("PORT")))
 }
