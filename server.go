d0e72cdbea88d02490dc6f9457201e4bb9e4f8dd
chawintee <chawinteetea@gmail.com>
2023-01-02 20:03:18 +0700

    [ADD] graceful shutdown

--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/chawintee/assessment/expense"
 	"github.com/labstack/echo/v4"
@@ -21,5 +25,20 @@ func main() {
 	e.PUT("/expenses/:id", expense.UpdateExpenseHandler)
 	e.GET("/expenses", expense.GetExpensesHandler)
 
-	log.Fatal(e.Start(os.Getenv("PORT")))
+	go func() {
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed { // Start server
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	log.Println("graceful-shutdown")
+
 }
