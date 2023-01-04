3ff9f778d245349a28fdae43c0d2e4234a84f42c
chawintee <chawinteetea@gmail.com>
2023-01-04 21:13:25 +0700

    [ADD] TestITCreateExpenses

---
+++ handle/handle_it_test.go
@@ -0,0 +1,76 @@
+//go:build integration
+// +build integration
+
+package handle
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+const serverPort = 80
+
+func TestITCreateExpenses(t *testing.T) {
+
+	// Setup server
+	eh := echo.New()
+	go func(e *echo.Echo) {
+		db, err := sql.Open("postgres", "postgresql://postgres:postgres@db/gokbtg?sslmode=disable")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		h := NewApplication(db)
+
+		e.POST("/expenses", h.CreateExpenses)
+		e.Start(fmt.Sprintf(":%d", serverPort))
+	}(eh)
+	for {
+		conn, err := net.DialTimeout("tcp", fmt.Sprintf("localhost:%d", serverPort), 30*time.Second)
+		if err != nil {
+			log.Println(err)
+		}
+		if conn != nil {
+			conn.Close()
+			break
+		}
+	}
+	// Arrange
+	reqBody := `{"title":"Expense 1","amount":100,"note":"Note for expense 1","tags":["tag1","tag2"]}`
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("http://localhost:%d/expenses", serverPort), strings.NewReader(reqBody))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	client := http.Client{}
+
+	// Act
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	// Assertions
+	expected := "{\"id\":1,\"title\":\"Expense 1\",\"amount\":100,\"note\":\"Note for expense 1\",\"tags\":[\"tag1\",\"tag2\"]}"
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusCreated, resp.StatusCode)
+		assert.Equal(t, expected, strings.TrimSpace(string(byteBody)))
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = eh.Shutdown(ctx)
+	assert.NoError(t, err)
+}
