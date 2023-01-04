e9b7cc3060968544ba1a46111d73d4034fac94a7
chawintee <chawinteetea@gmail.com>
2023-01-04 07:53:58 +0700

    [ADD] TestUpdateExpenses

--- handle/handle_test.go
+++ handle/handle_test.go
@@ -1,3 +1,6 @@
+//go:build unit
+// +build unit
+
 package handle
 
 import (
@@ -12,35 +15,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetExpenses(t *testing.T) {
+func TestCreateExpenses(t *testing.T) {
 	// Arrange
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/expenses", strings.NewReader(""))
+	mockTestBody := `{"id":1,"title":"Expense 1","amount":100,"note":"Note for expense 1","tags":["tag1","tag2"]}`
+	req := httptest.NewRequest(http.MethodPost, "/expenses", strings.NewReader(mockTestBody))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 
-	newsMockRows := sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
-		AddRow("1", "Expense 1", 100, "Note for expense 1", `{tag1,tag2}`).
-		AddRow("2", "Expense 2", 50, "Note for expense 2", `{tag3,tag4}`)
+	newsMockRows := sqlmock.NewRows([]string{"id"}).AddRow("1")
 
 	db, mock, err := sqlmock.New()
-	mock.ExpectPrepare(regexp.QuoteMeta(`SELECT id, title, amount, note, tags FROM expenses`)).
-		ExpectQuery().
+	mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO expenses (title, amount, note, tags) values ($1, $2, $3, $4)  RETURNING id`)).
+		WithArgs("Expense 1", 100, "Note for expense 1", `{"tag1","tag2"}`).
 		WillReturnRows(newsMockRows)
-
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
-	expected := "[{\"id\":1,\"title\":\"Expense 1\",\"amount\":100,\"note\":\"Note for expense 1\",\"tags\":[\"tag1\",\"tag2\"]},{\"id\":2,\"title\":\"Expense 2\",\"amount\":50,\"note\":\"Note for expense 2\",\"tags\":[\"tag3\",\"tag4\"]}]"
+	expected := "{\"id\":1,\"title\":\"Expense 1\",\"amount\":100,\"note\":\"Note for expense 1\",\"tags\":[\"tag1\",\"tag2\"]}"
 	h := handler{db}
 	c := e.NewContext(req, rec)
-
 	// Act
-	err = h.GetExpenses(c)
+	err = h.CreateExpenses(c)
 
 	// Assertions
 	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, http.StatusCreated, rec.Code)
 		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 	}
 }
@@ -81,7 +81,7 @@ func TestGetExpense(t *testing.T) {
 	}
 }
 
-func TestCreateExpenses(t *testing.T) {
+func TestUpdateExpenses(t *testing.T) {
 	// Arrange
 	e := echo.New()
 	mockTestBody := `{"id":1,"title":"Expense 1","amount":100,"note":"Note for expense 1","tags":["tag1","tag2"]}`
@@ -89,24 +89,67 @@ func TestCreateExpenses(t *testing.T) {
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 
-	newsMockRows := sqlmock.NewRows([]string{"id"}).AddRow("1")
+	newsMockRows := sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
+		AddRow("1", "Expense 1", 100, "Note for expense 1", `{tag1,tag2}`)
 
 	db, mock, err := sqlmock.New()
-	mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO expenses (title, amount, note, tags) values ($1, $2, $3, $4)  RETURNING id`)).
-		WithArgs("Expense 1", 100, "Note for expense 1", `{"tag1","tag2"}`).
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT id, title, amount, note, tags FROM expenses WHERE id = $1`)).WithArgs("1")
+
+	mock.ExpectPrepare(regexp.QuoteMeta(`UPDATE expenses SET title=$1, amount=$2, note=$3, tags=$4 WHERE id=$5`)).
+		ExpectQuery().
+		WithArgs("Expense 1", 100, "Note for expense 1", `{"tag1","tag2"}`, "1").
 		WillReturnRows(newsMockRows)
+
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	expected := "{\"id\":1,\"title\":\"Expense 1\",\"amount\":100,\"note\":\"Note for expense 1\",\"tags\":[\"tag1\",\"tag2\"]}"
 	h := handler{db}
 	c := e.NewContext(req, rec)
+	c.SetPath("/expenses/:id")
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+
 	// Act
-	err = h.CreateExpenses(c)
+	err = h.UpdateExpenses(c)
 
 	// Assertions
 	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
+	}
+}
+
+func TestGetExpenses(t *testing.T) {
+	// Arrange
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/expenses", strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	newsMockRows := sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
+		AddRow("1", "Expense 1", 100, "Note for expense 1", `{tag1,tag2}`).
+		AddRow("2", "Expense 2", 50, "Note for expense 2", `{tag3,tag4}`)
+
+	db, mock, err := sqlmock.New()
+	mock.ExpectPrepare(regexp.QuoteMeta(`SELECT id, title, amount, note, tags FROM expenses`)).
+		ExpectQuery().
+		WillReturnRows(newsMockRows)
+
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	expected := "[{\"id\":1,\"title\":\"Expense 1\",\"amount\":100,\"note\":\"Note for expense 1\",\"tags\":[\"tag1\",\"tag2\"]},{\"id\":2,\"title\":\"Expense 2\",\"amount\":50,\"note\":\"Note for expense 2\",\"tags\":[\"tag3\",\"tag4\"]}]"
+	h := handler{db}
+	c := e.NewContext(req, rec)
+
+	// Act
+	err = h.GetExpenses(c)
+
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 	}
 }
