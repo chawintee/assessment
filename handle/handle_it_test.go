7bc454ab7f1f6b36b42b2d830b78d8f893cb0fcc
chawintee <chawinteetea@gmail.com>
2023-01-04 21:40:10 +0700

    [ADD] TestITGetExpenses

--- handle/handle_it_test.go
+++ handle/handle_it_test.go
@@ -182,3 +182,56 @@ func TestITUpdateExpenses(t *testing.T) {
 	err = eh.Shutdown(ctx)
 	assert.NoError(t, err)
 }
+
+func TestITGetExpenses(t *testing.T) {
+	// Setup server
+	eh := echo.New()
+	go func(e *echo.Echo) {
+		db, err := sql.Open("postgres", "postgresql://postgres:postgres@db/gokbtg?sslmode=disable")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		h := NewApplication(db)
+
+		e.GET("/expenses", h.GetExpenses)
+		e.Start(fmt.Sprintf(":%d", serverPort))
+	}(eh)
+	for {
+		conn, err := net.DialTimeout("tcp", fmt.Sprintf("localhost:%d", serverPort), 30*time.Second)
+		if err != nil {
+			log.Println(err)
+		}
+		if conn != nil {
+			conn.Close()
+			break
+		}
+	}
+	// Arrange
+	reqBody := ``
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d/expenses", serverPort), strings.NewReader(reqBody))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	client := http.Client{}
+
+	// Act
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	// Assertions
+	expected := "[{\"id\":1,\"title\":\"Expense 3\",\"amount\":300,\"note\":\"Note for expense 3\",\"tags\":[\"tag3\",\"tags4\"]}]"
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.Equal(t, expected, strings.TrimSpace(string(byteBody)))
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = eh.Shutdown(ctx)
+	assert.NoError(t, err)
+}
